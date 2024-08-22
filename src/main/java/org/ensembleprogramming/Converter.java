160316f67f79f1999b9940aa1942a29c1183475c
Simon Harrer <simon.harrer@gmail.com>
2024-08-22 16:59:07 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/java/org/ensembleprogramming/Converter.java

--- src/main/java/org/ensembleprogramming/Converter.java
+++ src/main/java/org/ensembleprogramming/Converter.java
@@ -2,6 +2,7 @@ package org.ensembleprogramming;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
 
 public class Converter {
@@ -10,9 +11,11 @@ public class Converter {
     List<List<String>> districts = CsvReader.parseCsv(Path
         .of("csv/all.csv"));
     System.out.println(districts);
+    HashMap<String, String> map = new HashMap<>();
     for (List<String> district : districts) {
-
+      map.put(district.get(0), district.get(1));
     }
+    System.out.println(map);
   }
 
 
