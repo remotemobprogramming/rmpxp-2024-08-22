1549f62802b8aaafaf542abbe7cedc27ea825bb0
Simon Harrer <simon.harrer@gmail.com>
2024-08-22 16:50:21 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/java/org/ensembleprogramming/Converter.java

---
+++ src/main/java/org/ensembleprogramming/Converter.java
@@ -0,0 +1,14 @@
+package org.ensembleprogramming;
+
+import Path;
+
+public class Converter {
+
+  public static void main(String[] args) {
+    List<List<String>> districts = CsvReader.parseCsv(Path.of("all.csv"));
+
+  }
+
+
+
+}
