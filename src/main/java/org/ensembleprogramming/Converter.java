46a4168b4dcdb424eb1641fb76e0c3114f153fc7
Simon Harrer <simon.harrer@gmail.com>
2024-08-22 16:54:44 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/java/org/ensembleprogramming/Converter.java

--- src/main/java/org/ensembleprogramming/Converter.java
+++ src/main/java/org/ensembleprogramming/Converter.java
@@ -1,12 +1,18 @@
 package org.ensembleprogramming;
 
-import Path;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
 
 public class Converter {
 
-  public static void main(String[] args) {
-    List<List<String>> districts = CsvReader.parseCsv(Path.of("all.csv"));
+  public static void main(String[] args) throws IOException {
+    List<List<String>> districts = CsvReader.parseCsv(Path
+        .of("csv/all.csv"));
+    System.out.println(districts);
+    for (List<String> district : districts) {
 
+    }
   }
 
 
